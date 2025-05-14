401e49b3a708b6ce412c29e508db2450d164718c
coletrammer <cole.trammer@gmail.com>
2025-05-13 22:25:01 -0700

    client: detect the outer terminal's ability to grapheme cluster
    This is annoyingly but unsurprisingly complicated, by the fact that
    we can't just use mode 2027.

--- lib/src/feature.cpp
+++ lib/src/feature.cpp
@@ -29,6 +29,7 @@ class FeatureDetector {
 public:
     auto done() const -> bool { return m_done; }
     auto result() const -> Feature { return m_result; }
+    auto need_to_disable_mode_2027() const { return m_need_to_restore_mode_2027; }
 
     void handle_event(KeyEvent const&) {}
     void handle_event(MouseEvent const&) {}
@@ -44,6 +45,9 @@ public:
                     reply.support == terminal::ModeSupport::Set || reply.support == terminal::ModeSupport::Unset;
                 if (mode == terminal::DecMode::GraphemeClustering) {
                     is_supported |= reply.support == terminal::ModeSupport::AlwaysSet;
+                    if (reply.support == terminal::ModeSupport::Unset) {
+                        m_need_to_restore_mode_2027 = true;
+                    }
                 }
                 if (is_supported) {
                     m_result |= feature;
@@ -53,6 +57,23 @@ public:
     }
 
     void handle_event(terminal::CursorPositionReport const& report) {
+        m_cursor_reports++;
+        if (m_cursor_reports == 1) {
+            // Basic grapheme cluster check. The ZWJ emoji sequence
+            // should have width 2.
+            if (report.col == 2) {
+                m_result |= Feature::BasicGraphemeClustering;
+            }
+            return;
+        }
+        if (m_cursor_reports == 2) {
+            // The full grapheme cluster check expects width 1. See
+            // below for a detailed explanation.
+            if (report.col == 1) {
+                m_result |= Feature::FullGraphemeClustering;
+            }
+            return;
+        }
         if (report != m_prev_cursor) {
             // Upgrade kitty text sizing support
             if (m_prev_cursor) {
@@ -81,6 +102,8 @@ private:
 
     // State for detecting text sizing protocol.
     di::Optional<terminal::CursorPositionReport> m_prev_cursor;
+    u32 m_cursor_reports { 0 };
+    bool m_need_to_restore_mode_2027 { false };
 };
 
 auto detect_features(dius::SyncFile& terminal) -> di::Result<Feature> {
@@ -96,13 +119,59 @@ auto detect_features(dius::SyncFile& terminal) -> di::Result<Feature> {
 
     // Undercurl support query (this sets urderline mode=3 (undercurl) and then requests what the
     // terminal currently thinks the graphics attributes are).
-    di::writer_print<di::String::Encoding>(request_buffer, "\x1b[0m\x1b[4:3m\x1bP$qm\x1b\\\\\x1b[0m"_sv);
+    di::writer_print<di::String::Encoding>(request_buffer, "\x1b[0m\x1b[4:3m\x1bP$qm\x1b\\\x1b[0m"_sv);
 
     // Kitty keyboard protocol query
     di::writer_print<di::String::Encoding>(request_buffer, "\033[?u"_sv);
 
+    // Grapheme clustering support query
+    // Although DEC mode 2027 exists, it isn't all too helpful because as of this writing,
+    // all known terminals which implement mode 2027 do not fully support Unicode 16 grapheme
+    // clustering. Additionally, kitty 0.42.0 supports grapheme clustering but does not advertise
+    // support for mode 2027 (and it behaves slightly different anyway, because of its handling
+    // of variation selector 15).
+    //
+    // We implement the Unicode handling specified by
+    // [Kitty](https://github.com/kovidgoyal/kitty/blob/master/docs/text-sizing-protocol.rst#the-algorithm-for-splitting-text-into-cells),
+    // which is mostly aligned with the spec for mode 2027. However, if we split cells using
+    // the kitty spec we need to adjust our rendering to handle edge cases in outer terminals
+    // which "support" mode 2027 but don't fully agree with us. We also need to check for terminals
+    // with legacy behavior which don't bother doing any grapheme clustering, as in those cases
+    // its better to disable support for mode 2027 ourselves.
+    //
+    // As a side note: the explicit text sizing protocol by kitty completely fixes this issues,
+    // and so it will be used by our rendering if possible.
+    //
+    // To detect basic support for grapheme clustering we first enable mode 2027, and then
+    // send a black cat emoji (which has a ZWJ).
+    //
+    // To detect conformance with the kitty Unicode spec I found one of the automated tests
+    // which fail on all known terminals.
+    //
+    // Test 815:
+    // ÷ [0.2] LATIN SMALL LETTER A (Other) ÷ [999.0] ARABIC NUMBER SIGN (Prepend) × [9.2] LATIN SMALL LETTER B (Other)
+    // According to the kitty spec, this only moves the cursor by 1 cells, because the arabic number sign has width 0
+    // and thus is placed into the first cell, and 'b' gets placed into the same cell because there is no break between
+    // it and the arabic number sign. This was tested in the following terminals which have grapheme clustering support
+    // and all of them failed:
+    // - kitty 0.40.1
+    // - ghostty 1.1.3
+    // - contour 0.5.1
+    // - foot 1.21.0
+    // - wezterm 0-unstable-2025-02-23
+    //
+    // Obviously, other terminals which don't support graphemes at all will fail. The following known terminals pass
+    // this check:
+    // - kitty 0.42.0
+    // - ttx (when running in a terminal with some support for grapheme clustering)
+
+    // Start by doing the basic test
+    di::writer_print<di::String::Encoding>(request_buffer, "\033[?2027h"_sv);
+    di::writer_print<di::String::Encoding>(request_buffer, u8"\r\033[K🐈‍⬛\033[6n"_sv);
+    di::writer_print<di::String::Encoding>(request_buffer, u8"\r\033[Ka\u0600b\033[6n"_sv);
+
     // Text sizing protocol query
-    di::writer_print<di::String::Encoding>(request_buffer, "\033[6n"_sv);
+    di::writer_print<di::String::Encoding>(request_buffer, "\r\033[K\033[6n"_sv);
     di::writer_print<di::String::Encoding>(request_buffer, "{}"_sv,
                                            terminal::OSC66 { .info = { .width = 2 }, .text = "a"_sv }.serialize());
     di::writer_print<di::String::Encoding>(request_buffer, "\033[6n"_sv);
@@ -141,6 +210,12 @@ auto detect_features(dius::SyncFile& terminal) -> di::Result<Feature> {
         }
     }
 
-    return detector.result();
+    auto result = detector.result();
+    if (detector.need_to_disable_mode_2027()) {
+        // Be a good citizen, and restore the mode. We need to enable it to properly
+        // test the implementation.
+        di::writer_print<di::String::Encoding>(terminal, "\033[?2027l"_sv);
+    }
+    return result;
 }
 }
